8bb3108dffc39dfc07d5ef81b134ec5b73695566
Huy <huynvk@gmail.com>
2021-09-10 16:56:20 +0700

    Init material ui

---
+++ src/config/theme.js
@@ -0,0 +1,19 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      dark: '#E1B440',
+      light: '#E1B440',
+      main: '#E1B440',
+    },
+    secondary: {
+      dark: '#E1B440',
+      light: '#E1B440',
+      main: '#E1B440',
+    },
+  },
+  spacing: 4,
+});
+
+export default theme;
